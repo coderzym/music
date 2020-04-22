55e43588ee089f990053d90a6dcc73831fe6207f
coderzym <zhaoyiming035@gmail.com>
2020-04-22 13:10:58 +0800

    修复若干BUG

--- public/js/login.js
+++ public/js/login.js
@@ -14,21 +14,63 @@ let log = $('.last')[0],
     logTop = $('.login', parent.document).children('.top')[0],
     value1 = $('.tel').children('input')[0],
     value2 = $('.pwd')[0],
-    userImg = $('.last').find('img')[0],
-    out = $('.logout'),
+    main2log = $('.r-list-top').children('a'),
+    out = $('.logout')
+
+// 定义一个登录框显示隐藏的开关，定义是否更新用户信息的开关
+let loginFlag = false,
+    userFlag = true
+
+let userImg = $('.last').find('img')[0],
     userInfo = $('.user-info'),
     menu = $('.last').children('span'),
-    main2log = $('.r-list-top').children('a'),
     right2log = $('.r-list-top'),
     logIn = $('.logIn'),
     logInImg = $('.log-info').children('img'),
     eventCount = $('.eventCount'),
     follows = $('.follows'),
     followeds = $('.followeds'),
-    userInfoP = $('.userInfo').children('p')
+    userInfoP = $('.userInfo').children('p'),
+    tRight = $('.t-right')
 
-// 定义一个登录框显示隐藏的开关，定义是否更新用户信息的开关
-let loginFlag = false, userFlag = false
+// 登录后的状态
+function showLogin(userData) {
+    let profile = userData.profile
+    $(userImg).attr({
+        'src': profile.avatarUrl,
+        'alt': profile.nickname
+    })
+    $(userImg).css('display', 'block')
+    $(menu).css('display', 'none')
+    $(userImg).mouseenter(() => {
+        $(userInfo).css('display', 'flex')
+    })
+    $(userInfo).mouseleave(() => {
+        $(userInfo).css('display', 'none')
+    })
+    $(tRight).mouseleave(() => {
+        $(userInfo).css('display', 'none')
+    })
+    $(right2log).css('display', 'none')
+    $(logIn).css('display', 'block')
+    $(logInImg).attr('src', profile.avatarUrl)
+    $(followeds).text(profile.followeds)
+    $(follows).text(profile.follows)
+    $(eventCount).text(profile.eventCount)
+    $(userInfoP).text(profile.nickname)
+}
+
+// 退出登录
+function hideLogin() {
+    $(userImg).attr({
+        'src': '',
+        'alt': ''
+    })
+    $(userImg).css('display', 'none')
+    $(menu).css('display', 'block')
+    $(right2log).css('display', 'block')
+    $(logIn).css('display', 'none')
+}
 
 // 警告框的弹出与消失
 function warn(value) {
@@ -82,8 +124,15 @@ if (main2log) {
     $(main2log).click(loginFn)
 }
 
-// 点击页面上的登录后，登录框显示
-$(log).click(loginFn)
+if (!localStorage['isLogin']) {
+    localStorage.setItem('isLogin', false)
+}
+
+let localIsLogin = JSON.parse(localStorage.getItem('isLogin'))
+
+if (!localIsLogin) {
+    $(log).on('click', loginFn)
+}
 
 // 点击关闭按钮登录框隐藏
 $(clo).click(loginFn)
@@ -97,21 +146,36 @@ function preventKeyboard() {
     }
 }
 
-// 点击登录显示登录界面
+// 判断复选框是否选中
+function isChecked(el) {
+    var a1 = $(el).prop("checked")
+    return a1
+}
+
+// 绑定点击事件
 $(log1).click(() => {
-    var a1 = $(arg1).prop("checked")
-    if (a1) {
-        $(logShow).show()
-        $(contain).hide()
+    let res = isChecked($(arg1))
+    if (res) {
+        change(logShow, contain, true)
     } else {
         warn('请仔细阅读下方协议后勾选')
     }
 })
 
+// 根据用户选择切换不同的登录框
+function change(el1, el2, isLogin) {
+    if (isLogin) {
+        $(el1).show()
+        $(el2).hide()
+    } else {
+        $(el2).show()
+        $(el1).hide()
+    }
+}
+
 // 点击其他验证方式返回
 $(back).click(() => {
-    $(logShow).hide()
-    $(contain).show()
+    change(logShow, contain, false)
 })
 
 // 导入手机登录/退出登录API
@@ -129,20 +193,36 @@ $(log2).click(() => {
             resolve(logPhone(v1, v2))
         }).then(value => {
             warn('登录成功！')
+            $(log).off('click')
             // 如果用户信息存在于session里面，则直接更新
             if (sessionStorage['userInfo']) {
                 update()
             } else {
                 // 存储sessionStorage
                 saveInfo(value)
-                // 第一次更新页面
-                location.reload()
                 update()
             }
+            localStorage.setItem('isLogin', true)
+            location.reload()
         }).catch(() => {
             warn('用户名或者密码有误，请确认后重新输入')
         })
     }
+})
+
+// 更新用户退出后的页面
+$(out).click(e => {
+    e.stopPropagation()
+    new Promise(resolve => {
+        resolve(logout())
+    }).then(value => {
+        if (value) {
+            update()
+            sessionStorage.clear()
+            localStorage.setItem('isLogin', false)
+            location.reload()
+        }
+    })
 })
 
 // 保存用户的登录信息，避免刷新页面数据消失
@@ -157,53 +237,15 @@ if (sessionStorage['userInfo']) {
 
 // 更新用户登录后页面的入口函数
 function update() {
-    var userData = JSON.parse(sessionStorage.getItem('userInfo'))
+    let userData = JSON.parse(sessionStorage.getItem('userInfo'))
     // 界面显示用户基本信息
-    userFlag = !userFlag
     if (userFlag) {
-        $(userImg).attr({
-            'src': userData.profile.avatarUrl,
-            'alt': userData.profile.nickname
-        })
-        $(userImg).css('display', 'block')
-        $(menu).css('display', 'none')
-        $(userImg).mouseover(() => {
-            $(userInfo).css('display', 'flex')
-        })
-        $(userInfo).mouseleave(() => {
-            $(userInfo).css('display', 'none')
-        })
-        $(right2log).css('display', 'none')
-        $(logIn).css('display', 'block')
-        $(logInImg).attr('src', userData.profile.avatarUrl)
-        $(followeds).text(userData.profile.followeds)
-        $(follows).text(userData.profile.follows)
-        $(eventCount).text(userData.profile.eventCount)
-        $(userInfoP).text(userData.profile.nickname)
+        showLogin(userData)
     } else {
-        $(userImg).attr({
-            'src': '',
-            'alt': ''
-        })
-        $(userImg).css('display', 'none')
-        $(menu).css('display', 'block')
-        $(right2log).css('display', 'block')
-        $(logIn).css('display', 'none')
+        hideLogin()
     }
+    userFlag = !userFlag
 }
-
-// 更新用户退出后的页面
-$(out).click(e => {
-    e.stopPropagation()
-    new Promise(resolve => {
-        resolve(logout())
-    }).then(value => {
-        if (value) {
-            update()
-            sessionStorage.clear()
-        }
-    })
-})
 
 // 判断用户输入格式是否正确
 const test = {
